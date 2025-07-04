9d5c399e24e7f31cc8a810cfd984b3b5005fce90
Sean Cooper <sean@seantcooper.com>
2025-07-04 13:41:37 +0100

    Switching maps and R to replay

--- Breaker/ServiceWorker.js
+++ Breaker/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "pigbrain.io-Breaker-0.1.0";
 const contentToCache = [
-    "Build/30b9f7da689873237e431a07a028c5c0.loader.js",
+    "Build/f047c5d22256e1476ed3b1891816a981.loader.js",
     "Build/36bce4cf1f921a2aa879f11047758bc2.framework.js.unityweb",
-    "Build/9d01b137be6e70cdf1cc0e50ccc65c0a.data.unityweb",
-    "Build/dbdb34dacc93a657c5ece3a495f7037b.wasm.unityweb",
+    "Build/0d28184fbde95691db9a83f3d13b44e8.data.unityweb",
+    "Build/87f2e2e57917a8c18e3c0f7a22ae7414.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
