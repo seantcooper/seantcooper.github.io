0b17138dee925ff3bcb2edbbca7efc57b16a250d
Sean Cooper <sean@seantcooper.com>
2025-07-04 10:57:13 +0100

    New Breaker version

---
+++ Breaker/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "pigbrain.io-Breaker-0.1.0";
+const contentToCache = [
+    "Build/bbb7f2583dfc2f479bcfef0d3baa0e1e.loader.js",
+    "Build/36bce4cf1f921a2aa879f11047758bc2.framework.js.br",
+    "Build/9060fd86bb5f097495a482718b4663fc.data.br",
+    "Build/f2e6d9a647ef7b6c90987495a99c37ac.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
