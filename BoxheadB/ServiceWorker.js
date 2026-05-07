1a936052443a7c9d182ab87340b15036cde6b028
Sean Cooper <sean@seantcooper.com>
2026-05-07 21:30:42 +0100

    A (No Brotli) & B (Broli)

---
+++ BoxheadB/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "pigbrain.io-Boxhead-0.3.2";
+const contentToCache = [
+    "Build/BoxheadB.loader.js",
+    "Build/BoxheadB.framework.js.unityweb",
+    "Build/BoxheadB.data.unityweb",
+    "Build/BoxheadB.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
