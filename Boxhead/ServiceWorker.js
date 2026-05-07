eb7653f9904b65a4fd1f2f5d1714c9322ba762da
Sean Cooper <sean@seantcooper.com>
2026-05-07 20:29:15 +0100

    0.2.117

--- Boxhead/ServiceWorker.js
+++ Boxhead/ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "pigbrain.io-Boxhead-0.2.102";
+const cacheName = "pigbrain.io-Boxhead-0.2.117";
 const contentToCache = [
-    "Build/01a4777b383e60ee147865336d2df346.loader.js",
-    "Build/d5a10dc7c1170368f6476a6656d32c00.framework.js.unityweb",
-    "Build/40cd6a7a3986ed34781645a7707f1b33.data.unityweb",
-    "Build/7e3d403e9c4566aa8ccf2468308356c0.wasm.unityweb",
+    "Build/Boxhead.loader.js",
+    "Build/Boxhead.framework.js.br",
+    "Build/Boxhead.data.br",
+    "Build/Boxhead.wasm.br",
     "TemplateData/style.css"
 
 ];
