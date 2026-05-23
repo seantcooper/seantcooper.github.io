a878f6dd7040575b78a52a33f7a6ed197a26b364
Sean Cooper <sean@seantcooper.com>
2026-05-23 15:48:23 +0100

    Build Skilsort-0.1.18

--- B/ServiceWorker.js
+++ B/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "pigbrain.io-Skilsort-0.1.17";
+const cacheName = "pigbrain.io-Skilsort-0.1.18";
 const contentToCache = [
     "Build/A.loader.js",
     "Build/A.framework.js.unityweb",
